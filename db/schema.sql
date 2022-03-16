d17170bf5aefaddbdfb8a7986d9ab3388ce20e9f
Michael Bee <bee.michael.02@gmail.com>
2022-03-16 16:52:04 -0500

    Initial commit.

---
+++ db/schema.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS employee_cms_db;
+CREATE DATABASE employee_cms_db;
+
+USE employee_cms_db;
+
+CREATE TABLE departments (
+    dept_id INT NOT NULL AUTO_INCREMENT,
+    deptartment VARCHAR(30) NOT NULL PRIMARY KEY
+    
+    ON DELETE SET NULL
+);
+
+CREATE TABLE roles (
+    role_id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    deptartment VARCHAR(30) NOT NULL,
+    salary INT NOT NULL,
+
+    FOREIGN KEY (department)
+    REFERENCES department(id)
+    
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    emp_id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    deptartment VARCHAR(30) NOT NULL,
+    salary INT NOT NULL,
+    manager VARCHAR(30),
+
+    FOREIGN KEY (department)
+    REFERENCES department(id)
+    
+    ON DELETE SET NULL
+);
---
+++ index.js
@@ -0,0 +1,28 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'rootuser',
+    database: 'employees_db'
+  },
+);
+
+//An array of questions for user input
+//Need to make a bunch of different arrays that can redirect to based on user responses
+const welcome = [
+    {
+        
+    },
+];
+
+//Prompts user using the question array
+inquirer.prompt(welcome)
+    .then((answers) => {
+        console.log(answers);
+    }
+);
