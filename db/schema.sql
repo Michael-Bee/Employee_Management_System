ab0362709e5c239a29efabe9c4998643daaf3fa3
Michael Bee <bee.michael.02@gmail.com>
2022-03-16 23:01:08 -0500

    View All Selections done.

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,15 @@ CREATE DATABASE employee_cms_db;
 
 USE employee_cms_db;
 
+DROP TABLE IF EXISTS departments;
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
     ON DELETE SET NULL
 );
 
-CREATE TABLE role (
+DROP TABLE IF EXISTS roles;
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     deptartment_id INT NOT NULL REFERENCES department(id),
@@ -17,12 +19,12 @@ CREATE TABLE role (
     ON DELETE SET NULL
 );
 
-CREATE TABLE employee (
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL REFERENCES role(id),
     manager_id INT REFERENCES employee(id)
-   
     ON DELETE SET NULL
 );
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+const figlet = require('figlet');
 
 
 // Connect to database
@@ -13,11 +14,24 @@ const db = mysql.createConnection(
   },
 );
 
+//figlet intro
+console.log(figlet.textSync(
+  'Employee Management System',
+  {
+    font: 'speed',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 200,
+    whitespaceBreak: true
+  }
+))
 
+//Start Up
 function init(){
   initialPrompt();
 }
 
+//First Prompt
 function initialPrompt(){
 inquirer.prompt([
   {
@@ -28,31 +42,51 @@ inquirer.prompt([
   }
 ])
   .then((selection) => {
-    console.log(selection);
+    console.log(`You chose ${selection}.`);
 
-    //if then elses depending on selection
+//Second Prompt based on selection
+    //"View All" Selections
     if (selection === "View All Departments") {
-    initialPrompt();
+      db.query(`SELECT * FROM departments;`, function (err, results) {
+        console.table(results);
+      });
+      initialPrompt();
     }
+
     else if (selection === "View All Roles") {
+      db.query(`SELECT * FROM roles;`, function (err, results) {
+        console.table(results);
+      });
       initialPrompt();
     }
+
     else if (selection === "View All Employees") {
+      db.query(`SELECT * FROM employees;`, function (err, results) {
+        console.table(results);
+      });
       initialPrompt();
     }
+
+    //"Add" Selections
     else if (selection === "Add Department") {
       initialPrompt();
     }
+
     else if (selection === "Add Role") {
       initialPrompt();
     }
+
     else if (selection === "Add Employee") {
       initialPrompt();
     }
+
+    //Other Selections
     else if (selection === "Update an employee") {
       initialPrompt();
     }
+
     else (selection === "Exit Program") {
+      console.log("Exiting the Employee Management System. Goodbye!")
       process.exit();
     }
   })}
